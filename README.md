97b7da44ff180ccc5cd63da30a87b47990d20570
Carlos Carmona <carlos@coderty.com>
2021-01-08 13:37:22 +0100

    build: 3.0.0
    * build: 3.0.0

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
+<p align="center">
+  <a href="http://runnerty.io">
+    <img height="257" src="https://runnerty.io/assets/header/logo-stroked.png">
+  </a>
+  <p align="center">A new way for processes managing</p>
+</p>
+
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency Status][david-badge]][david-badge-url] 
+<a href="#badge">
+  <img alt="code style: prettier" src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg">
+</a>
+
 # XML/JSON Parser executor for [Runnerty]:
+
+### Installation:
+Through NPM
+
+```bash
+npm i @runnerty/executor-parse-xml-json
+```
+
+You can also add modules to your project with [runnerty-cli]
+
+```bash
+npx runnerty-cli add @runnerty/executor-parse-xml-json
+```
+
+This command installs the module in your project, adds example configuration in your `config.json` and creates an example plan of use.
+
+If you have installed [runnerty-cli] globally you can include the module with this command:
+
+```bash
+rty add @runnerty/executor-parse-xml-json
+```
 
 ### Configuration sample:
 ```json
@@ -88,3 +121,11 @@ Options definitions for `json_options` and `xml_options` params can be found her
 [json_options]: https://github.com/Leonidas-from-XIV/node-xml2js#options
 [xml_options]: https://github.com/Leonidas-from-XIV/node-xml2js#options-for-the-builder-class
 [Runnerty]: http://www.runnerty.io
+[downloads-image]: https://img.shields.io/npm/dm/@runnerty/executor-parse-xml-json.svg
+[npm-url]: https://www.npmjs.com/package/@runnerty/executor-parse-xml-json
+[npm-image]: https://img.shields.io/npm/v/@runnerty/executor-parse-xml-json.svg
+[david-badge]: https://david-dm.org/runnerty/executor-parse-xml-json.svg
+[david-badge-url]: https://david-dm.org/runnerty/executor-parse-xml-json
+[config.json]: http://docs.runnerty.io/config/
+[plan.json]: http://docs.runnerty.io/plan/
+[runnerty-cli]: https://www.npmjs.com/package/runnerty-cli
--- index.js
+++ index.js
@@ -1,146 +1,88 @@
-"use strict";
+'use strict';
 
-const xml2js = require("xml2js");
-const fs = require("fs");
+const xml2js = require('xml2js');
+const fs = require('fs');
 
-const Execution = global.ExecutionClass;
+const Executor = require('@runnerty/module-core').Executor;
 
-class parseXmlJsonExecutor extends Execution {
+class parseXmlJsonExecutor extends Executor {
   constructor(process) {
     super(process);
   }
 
-  exec(params) {
-    let _this = this;
-    let endOptions = { end: "end" };
+  async exec(params) {
+    let endOptions = { end: 'end' };
 
-    function parseToJson(xml) {
-      return new Promise(function (resolve, reject) {
-        let parser = new xml2js.Parser(params.json_options);
-        parser.parseString(xml, function (err, result) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.stringify(result));
-          }
-        });
-      });
+    async function parseToJson(xml) {
+      try {
+        const parser = new xml2js.Parser(params.json_options);
+        const result = await parser.parseStringPromise(xml);
+        resolve(JSON.stringify(result));
+      } catch (err) {
+        throw err;
+      }
     }
 
     function parseToXml(json) {
-      return new Promise(function (resolve, reject) {
-        let builder = new xml2js.Builder(params.xml_options);
-        let result = builder.buildObject(JSON.parse(json));
-        resolve(result);
-      });
+      try {
+        const builder = new xml2js.Builder(params.xml_options);
+        const result = builder.buildObject(JSON.parse(json));
+        return result;
+      } catch (err) {
+        throw err;
+      }
     }
 
-    function evaluateResults(results) {
-      return new Promise(function (resolve, reject) {
-        if (params.output_file) {
-          fs.writeFile(params.output_file, results, "utf8", function (err) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve();
-            }
-          });
-        } else {
-          endOptions.data_output = results;
-          resolve();
-        }
-        _this.end(endOptions);
-      });
+    async function evaluateResults(results) {
+      try {
+        await fs.promises.writeFile(params.output_file, results, 'utf8');
+        endOptions.data_output = results;
+        this.end(endOptions);
+      } catch (err) {
+        throw err;
+      }
     }
 
-    if (params.to === "json") {
-      new Promise(function (resolve, reject) {
-        let xml = "";
-        if (params.xml) {
-          xml = params.xml;
-          resolve(xml);
-        } else if (params.xml_file) {
-          fs.readFile(params.xml_file, function (err, result) {
-            if (err) {
-              reject(err);
-            } else {
-              xml = result;
-              resolve(xml);
-            }
-          });
-        } else {
-          reject(new Error(`Parsing to ${params.to}, params xml: ${params.xml} or xml_file: ${params.xml_file} not set correctly`));
+    // MAIN:
+    if (params.to === 'json') {
+      try {
+        if (!params.xml && !params.xml_file) {
+          throw new Error(
+            `Parsing to ${params.to}, params xml: ${params.xml} or xml_file: ${params.xml_file} not set correctly`
+          );
         }
-      }).then(xml => {
-        parseToJson(xml)
-          .then(results => {
-            evaluateResults(results)
-              .catch(err => {
-                endOptions.end = "error";
-                endOptions.messageLog = `writeToOutputFileJson: ${err}`;
-                endOptions.err_output = `writeToOutputFileJson: ${err}`;
-                _this.end(endOptions);
-              });
-          })
-          .catch(err => {
-            endOptions.end = "error";
-            endOptions.messageLog = `parseToJson: ${err}`;
-            endOptions.err_output = `parseToJson: ${err}`;
-            _this.end(endOptions);
-          });
-      }).catch(err => {
-        endOptions.end = "error";
-        endOptions.messageLog = `readFileToJson: ${err}`;
-        endOptions.err_output = `readFileToJson: ${err}`;
-        _this.end(endOptions);
-      });
-
-    } else if (params.to === "xml") {
-      new Promise(function (resolve, reject) {
-        let json = "";
-        if (params.json) {
-          json = params.json;
-          resolve(json);
-        } else if (params.json_file) {
-          fs.readFile(params.json_file, function (err, result) {
-            if (err) {
-              reject(err);
-            } else {
-              json = result;
-              resolve(json);
-            }
-          });
-        } else {
-          reject(new Error(`Parsing to ${params.to}, params json: ${params.json} or json_file: ${params.json_file} not set correctly`));
+        let xml = params.xml || '';
+        if (params.xml_file) xml = await fs.promises.readFile(params.xml_file);
+        const results = await parseToJson(xml);
+        await evaluateResults(results);
+      } catch (err) {
+        endOptions.end = 'error';
+        endOptions.messageLog = err;
+        endOptions.err_output = err;
+        this.end(endOptions);
+      }
+    } else if (params.to === 'xml') {
+      try {
+        if (!params.json && !params.json) {
+          throw new Error(
+            `Parsing to ${params.to}, params json: ${params.xml} or json_file: ${params.xml_file} not set correctly`
+          );
         }
-      }).then(json => {
-        parseToXml(json)
-          .then(results => {
-            evaluateResults(results)
-              .catch(err => {
-                endOptions.end = "error";
-                endOptions.messageLog = `writeToOutputFileXml: ${err}`;
-                endOptions.err_output = `writeToOutputFileXml: ${err}`;
-                _this.end(endOptions);
-              });
-          })
-          .catch(err => {
-            endOptions.end = "error";
-            endOptions.messageLog = `parseToXml: ${err}`;
-            endOptions.err_output = `parseToXml: ${err}`;
-            _this.end(endOptions);
-          });
-      }).catch(err => {
-        endOptions.end = "error";
-        endOptions.messageLog = `readFileToXml: ${err}`;
-        endOptions.err_output = `readFileToXml: ${err}`;
-        _this.end(endOptions);
-      });
+        let json = params.json || '';
+        if (params.json_file) json = await fs.promises.readFile(params.json_file);
+        const results = parseToXml(json);
+        await evaluateResults(results);
+      } catch (err) {
+        endOptions.end = 'error';
+        endOptions.messageLog = err;
+        endOptions.err_output = err;
+        this.end(endOptions);
+      }
     } else {
-      endOptions.end = "error";
+      endOptions.end = 'error';
       endOptions.messageLog = `param 'to': ${params.to} not set correctly use to json/xml`;
       endOptions.err_output = `param 'to': ${params.to} not set correctly use to json/xml`;
-      _this.end(endOptions);
+      this.end(endOptions);
     }
   }
 }
